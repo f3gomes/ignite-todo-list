a54752585027a2794a09d8efd7693ee3e140acc0
fomes <fgomesdeluna@gmail.com>
2022-12-26 15:32:50 -0300

    create header app

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styles from "./Header.module.css";
+import logo from "../assets/logo.png";
+
+export default function Header() {
+  return (
+    <div className={styles.container}>
+      <img src={logo} alt="foguete" />
+      <h1>
+        <span>to</span>
+        <span>do</span>
+      </h1>
+    </div>
+  );
+}
